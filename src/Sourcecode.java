1975376ddb073c0219555f7a92627212224bbfc3
minqukanq <kang.mingu94@gmai.com>
2023-03-02 08:28:41 +0900

    저는 소스 코드 브랜치입니다.

--- src/Sourcecode.java
+++ src/Sourcecode.java
@@ -1,8 +1,9 @@
 public class Sourcecode {
 
     public void printSomething(String name){
-        System.out.println("저는 새로운 브랜치로 넘어온" + name + "입니다. 반갑습니다!");
-        System.out.println("Hello Source Code!!");
+        System.out.println("저는 소스 코드 브랜치입니다. 만나서 반갑습니다.");
+        System.out.println("Hello Source Code Branch!!");
+        System.out.println("print(Hello World)");
     }
 
     public static void main(String[] args) {
